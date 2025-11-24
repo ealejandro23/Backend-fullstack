24211e8eb0a1522f55d85425fdbfbd5eba5fe45d
FeOrtegah <fe.ortegah@duocuc.cl>
2025-11-24 15:57:35 -0300

    Update Imagen.java

--- demo/src/main/java/Proyecto_EFA/demo/model/Imagen.java
+++ demo/src/main/java/Proyecto_EFA/demo/model/Imagen.java
@@ -1,33 +1,43 @@
 package Proyecto_EFA.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "imagen")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
+@EqualsAndHashCode(exclude = "producto") 
 public class Imagen {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-	@Column(length = 255, nullable = false)
-	private String url;
+    @Column(name = "url", length = 1024, nullable = false)
+    private String url;
 
-	@ManyToOne
-	@JoinColumn(name = "producto_id")
-	@JsonIgnore
-	private Producto producto;
+    @ManyToOne
+    @JoinColumn(name = "producto_id", nullable = false)
+    @JsonIgnore
+    private Producto producto;
+
+    @Column(name = "public_id", length = 255) 
+    private String publicId;
+
+    public void setPublicId(String publicId) {
+        this.publicId = publicId;
+    }
 }
