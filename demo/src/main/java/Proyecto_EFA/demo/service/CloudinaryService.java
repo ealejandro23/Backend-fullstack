d09f04f654475b09270cd3ec3fe4b90c2b5c4d2a
FeOrtegah <fe.ortegah@duocuc.cl>
2025-11-24 15:42:07 -0300

    Update CloudinaryService.java

--- demo/src/main/java/Proyecto_EFA/demo/service/CloudinaryService.java
+++ demo/src/main/java/Proyecto_EFA/demo/service/CloudinaryService.java
@@ -1,1 +1,28 @@
+package Proyecto_EFA.demo.service;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+
+    @Autowired
+    private Cloudinary cloudinary;
+    public Map<?, ?> uploadImage(MultipartFile file) throws IOException {
+        // La carpeta 'efa_productos' se usa para organizar las imágenes en Cloudinary.
+        Map<?, ?> uploadResult = cloudinary.uploader().upload(
+            file.getBytes(), 
+            ObjectUtils.asMap("folder", "efa_productos") 
+        );
+        return uploadResult;
+    }
+    public void deleteImage(String publicId) throws IOException {
+      
+        cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+    }
+}
